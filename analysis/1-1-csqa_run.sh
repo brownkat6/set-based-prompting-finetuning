0603213fae5bf3cba270a4a6445858778b18bed6
Katrina <katrinabrown@college.harvard.edu>
2025-01-05 01:53:10 -0500

    set-based-prompting finetuning

--- analysis/1-1-csqa_run.sh
+++ analysis/1-1-csqa_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=csqa_quoted
-#SBATCH --partition=seas_gpu
-#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
+##SBATCH --partition=seas_gpu
+##SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
+#SBATCH --partition=gpu_requeue
+#SBATCH --gres=gpu:1
 #SBATCH --time=0-20:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=8
